195d57e38b77e6998457e0c49d864c11fdea84bb
Stuart Halloway <stu@thinkrelevance.com>
2008-03-14 15:47:51 -0400

    experiments with dired and completion for Command-T alternative interesting but less useful than just working with built-in tags

--- command_t.el
+++ command_t.el
@@ -1,6 +1,7 @@
+;;;; This is a set of experiments on possible Emacs variants of TextMates' Command-T
+
 ;; (string-matches "h.*t" '("hot" "post" "heat"))
 (defun string-matches (re list)
-  (message "Searching for %s" re)
   (let ((result nil))
     (dolist (item list)
       (if (string-match re item)
@@ -13,24 +14,27 @@
     ;; If we need to ask for the tag table, allow that.
     (let ((enable-recursive-minibuffers t))
       (visit-tags-table-buffer))
-    (string-matches (phoenix-search-expression string) (tags-ituntable-files))))
+    (string-matches (phoenix-search-re string) (tags-table-files))))
 
-;; build a path-component match expression
-;; (phoenix-path-component-expression "12")
-(defun phoenix-path-component-expression (string)
+(defun phoenix-last-path-component-re (string)
+  (concat (phoenix-path-component-re string) "$"))
+
+;; build a path-component match re
+;; (phoenix-path-component-re "12")
+(defun phoenix-path-component-re (string)
   (apply 'concat (phoenix-splice (split-string string "" t) "[^/]*")))
 
-;; build an expression like "1[^/*]2[.*/.*]/3[^/]*4" from "12/34"
-;; (phoenix-search-expression "12/34")
-;; (string-match (phoenix-search-expression "12/34") "01/123/789/345")
-(defun phoenix-search-expression (string)
+;; build an re like "1[^/*]2[.*/.*]/3[^/]*4" from "12/34"
+;; (phoenix-search-re "12/34")
+;; (string-match (phoenix-search-re "12/34") "01/123/789/345")
+(defun phoenix-search-re (string)
   (let ((names (split-string string "/" t)))
     (apply 'concat
-           (phoenix-splice (mapcar 'phoenix-path-component-expression names)
+           (phoenix-splice (mapcar 'phoenix-path-component-re names)
                            ".*/.*"))))
 
-;; (phoenix-relevance-expression "app/foo")
-(defun phoenix-relevance-expression (string)
+;; (phoenix-relevance-re "app/foo")
+(defun phoenix-relevance-re (string)
   (apply 'concat (phoenix-splice (split-string string "/" t) "[^/]*/[^/]*")))
 
 ;; (phoenix-splice '(1 2 3) :foo)
@@ -41,24 +45,28 @@
       (setf result (cons splice result)))
     (reverse (cdr result))))
 
+;; infer the top directory of a project from the location of the 
+;; current tags file. Is this a reasonable assumption?
+(defun phoenix-find-file-top-dir ()
+  (file-name-directory tags-file-name))
+
 (defun find-file-in-tag-files-dired (str)
   "Find files matching string, a la TextMate's Command-T"
   (interactive "s")
   (let ((matches (string-matches-in-tag-files str)))
-    (message "Matches are %s" matches)
     (if (not matches) (error "No matches"))    
     (switch-to-buffer (get-buffer-create "*Choose A File*"))
-    (erase-buffer)
-    (widen)
-    (kill-all-local-variables)
     (setq buffer-read-only nil)
-    (setq default-directory "~/relevance/surety/trunk/")
+    (widen)
+    (erase-buffer)
+    (kill-all-local-variables)
+    (setq default-directory (phoenix-find-file-top-dir))
     (let* ((files (apply 'call-process (append '("ls" nil t nil "-al") matches)))
            (ls-list (split-string (buffer-substring (point-min) (point-max)) "\n")))
       (erase-buffer)
      (dolist (item (phoenix-relevance-sort str ls-list))
        (insert-string (concat item "\n")))
-      (dired-mode "~/relevance/surety/trunk/")
+      (dired-mode (phoenix-find-file-top-dir))
       (set (make-local-variable 'dired-subdir-alist)
            (list (cons default-directory (point-min-marker))))
       (beginning-of-buffer))))
@@ -66,7 +74,7 @@
 ;; sort list by relevance to string
 ;; early match beats late match beats no match
 (defun phoenix-relevance-sort (str list)
-  (let ((rel (phoenix-relevance-expression str)))
+  (let ((rel (phoenix-relevance-re str)))
     (sort list
           (lambda (x y)
             (let ((rel-x (string-match rel x))
@@ -77,9 +85,27 @@
                (t (< rel-x rel-y))))))))
         
 
-;; (global-set-key "\M-t" 'find-file-in-tag-files-dired) 
+;; (global-set-key "\M-t" 'find-tag-file)
 
+;; this does not work as well as just searching by tags
+(defun find-tag-file ()
+  (interactive)
+  (visit-tags-table-buffer)
+  (find-file (completing-read
+   "Find a file: "
+   'phoenix-tabs-completion
+   nil t nil)))
 
+(defun phoenix-tabs-completion (string pred flag)
+  (let ((matches (string-matches-in-tag-files string)))
+    (cond
+     (flag matches)
+     ; does not yet handle the lambda case correctly
+     (t
+      (visit-tags-table-buffer)
+      (member string (tags-table-files))))))
 
+     
+          
 
 
